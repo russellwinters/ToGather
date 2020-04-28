1e2ecf21c556b5bfba1364c881d78e12cf30d7ef
RylanFerrer <rylanferrer@gmail.com>
2020-04-27 23:39:16 -0700

    added web socket functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Landing from "./pages/Landing";
@@ -6,7 +7,46 @@ import Games from "./pages/Games";
 import Exercise from "./pages/Exercise";
 import Random from "./pages/Random";
 
+const ENDPOINT = "http://localhost:5000";
+const socket = socketIOClient(ENDPOINT);
+
+
+
 function App() {
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [tests, setTest] = useState("");
+  useEffect(() =>{
+    socket.on('test', (data) => {
+      console.log(data)
+    })
+    socket.on('get all online', (data) => {
+      setOnlineUsers(data.names);
+    })
+    socket.on('user left', (data) => {
+      console.log(`${data.username} left`);
+      console.log(onlineUsers.filter(user => user !== data.username))
+      setOnlineUsers(onlineUsers.filter(user => user.username !== data.username));
+    })
+    socket.on('user joined', (data) => {
+      console.log(`${data.username} joined`);
+      setOnlineUsers([...onlineUsers, {username: data.username, id: data.id}]);
+    })
+    return () => {
+      socket.off('get all online');
+      socket.off("user left");
+      socket.off('user joined');
+    };
+  })
+  const [response, setResponse] = useState("");
+  const userList = onlineUsers.map((item) => {
+    return <h4 onClick = {() => console.log(item.id)} >{item.username}</h4>
+  })
+  const test = async(username) => {
+    socket.emit('add user', username);
+  }
+  const directMessage = async(message) => {
+    socket.emit('direct message', {id:onlineUsers[0].id, message })
+  }
   return (
     <Router className="App">
       <Header />
@@ -16,6 +56,16 @@ function App() {
         <Route path="/exercise" component={Exercise} />
         <Route path="/random" component={Random} />
       </Switch>
+      <input onChange = {(event) => setResponse(event.target.value) }>
+      </input>
+      <button onClick = {() => test(response)}>button</button>
+      <div>
+        <h5>Online Users</h5>
+        {userList}
+      </div>
+      <input onChange = {(event) => setTest(event.target.value)}></input>
+      <button onClick = {() => directMessage(tests)}>Hello</button>
+    
     </Router>
   );
 }
--- server.js
+++ server.js
@@ -1,27 +1,93 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const http = require("http");
+const socketIo = require("socket.io");
 const cors = require("cors");
 require("dotenv").config();
 
+const PORT = process.env.PORT || 5000;
 //middleware
 app.use(express.json());
 app.use(cors());
 
 //routes
 app.use("/api/games", require("./routes/game"));
-
+app.use("/api/calendar", require("./routes/calendar"))
 //Connect to Mongo
-mongoose.connect(
-  process.env.MONGODB_URI || process.env.Mongo_URI,
-  { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true },
-  () => {
-    console.log("connected to MongoDB");
-  }
-);
 
-//Listen
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+// commenting this out cause i dont have the env vars file yet
+// mongoose.connect(
+//   process.env.MONGODB_URI || process.env.Mongo_URI,
+//   { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true },
+//   () => {
+//     console.log("connected to MongoDB");
+//   }
+// );
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+let interval;
+let numUsers = 0;
+let names = [];
+io.on("connection", (socket) => {
+  let addedUser = false;
+  socket.emit('get all online', {
+    names
+  })
+  socket.on('new message', (data) => {
+    console.log(data)
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+  socket.on('direct message', (data) => {
+
+    socket.broadcast.to(data.id).emit("test",data.message)
+  })
+
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+    console.log(socket.id)
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    names.push({username: socket.username, id: socket.id});
+    addedUser = true;
+    console.log(names);
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      id: socket.id,
+      numUsers: numUsers
+    });
+  });
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+      names = names.filter(name => name !== socket.username)
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers,
+        names
+      });
+    }
+  });
 });
+
+const getApiAndEmit = socket => {
+  console.log(socket)
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
